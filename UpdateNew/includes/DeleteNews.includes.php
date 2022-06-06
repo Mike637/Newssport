57cd9208bcfc09de1fe1f9ab4cd583f03933cedf
misha25101991 <owen7@list.ru>
2022-06-07 01:54:34 +0300

    Update 07.06.2022

--- UpdateNew/includes/DeleteNews.includes.php
+++ UpdateNew/includes/DeleteNews.includes.php
@@ -1,6 +1,14 @@
 <?php
 
 
+require_once("../classes/DeleteNews.classes.php");
 
+
+
+
+
+ $objectDeleteNews = new DeleteNews;
+
+  $objectDeleteNews->NewsDelete();
 
  ?>
--- UpdateNew/js/jsUpdateNew.js
+++ UpdateNew/js/jsUpdateNew.js
@@ -1,38 +1,5 @@
+
 let id = document.location.search;
-
-/*
-class (includes folder) from PageNew
-*/
-function detailViewNew(idParameter)
-{
-  let container = document.querySelector(".container");
-  data = "";
-  fetch("../../PageNew/includes/PageNewView.includes.php"+idParameter,{
-   method:"GET",
-  }).
-  then(response => response.json()).
-  then(response =>  {
-    data+=`${response.img_title}`;
-data+=`<a href = "../../FormUpdateNew/templates/formUpdateNew.php${idParameter}">редактировать</a>`;
-data +=`<br>`;
-data+=`<br>`;
-data+=`<a data-id = "${response.img_id}" href="#">удалить</a>`;
-container.innerHTML = data;
-var hrefDelete = document.querySelector('a[data-id]');
-
-hrefDelete.onclick = function()
-{
-  var id = this.getAttribute("data-id");
-  fetch(`../includes/DeleteNews.includes.php${idParameter}`,
-  {
-    method:"GET";
-  }).
-  then(response => response.text()).
-  then(response => alert(response))
-}
-
-})
-}
 
 
 window.onload = function()
@@ -41,4 +8,52 @@ window.onload = function()
   detailViewNew(id);
 
 
+}
+
+async function detailViewNew(idParameter)
+{
+  let container = document.querySelector(".container");
+  data = "";
+  response = await fetch("../../PageNew/includes/PageNewView.includes.php"+idParameter,{
+   method:"GET",
+ });
+
+if (!response.ok)
+{
+  throw new Error(`Error in response!!! Your status is ${response.status}`)
+}
+  else {
+
+
+ jsonResponse = await response.json();
+ data+=`${jsonResponse.img_title}`;
+data+=`<a href = "../../FormUpdateNew/templates/formUpdateNew.php${idParameter}">редактировать</a>`;
+data +=`<br>`;
+data+=`<br>`;
+data+=`<a data-id = "${jsonResponse.img_id}" href="#">удалить</a>`;
+container.innerHTML = data;
+var hrefDelete = document.querySelector('a[data-id]');
+
+hrefDelete.onclick = async function()
+{
+
+
+
+  var id = this.getAttribute("data-id");
+  response = await fetch(`../includes/DeleteNews.includes.php${idParameter}`,
+  {
+    method:"GET",
+  });
+  if (!response.ok)
+  {
+    throw new Error(`Error in response!!! Your status is ${response.status}`)
+  }
+  else {
+    textResponse = await response.text();
+    alert(textResponse);
+    window.location.href = "../../Main/templates/main.php"
+  }
+
+}
+}
 }
